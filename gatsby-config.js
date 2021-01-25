8ca69dab5134c7a3850ba7bfb0ad110fda83b290
mfawcett83 <34173822+Motown83@users.noreply.github.com>
2021-01-24 19:57:47 -0800

    added EmotionJS, TypographyJS and placeholder layout/content

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,19 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
   plugins: [`gatsby-plugin-netlify-cms`],
-  plugins: [`gatsby-plugin-sass`]
+  plugins: [`gatsby-plugin-sass`],
+  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [`gatsby-plugin-emotion`],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
